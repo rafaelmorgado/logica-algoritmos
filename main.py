11794d1b4f21d1cbff08d8edef9d44013010d393
rafaelmorgado <ecrafaelmorgado@gmail.com>
2020-10-15 20:33:20 +0000

    commit

--- main.py
+++ main.py
@@ -7,40 +7,72 @@ Para atender a todos os clientes, cada cliente poderá levar apenas um dos tipos
 
 fruta = str(input("F-Filé Duplo, A-Alcatra, P-Picanha: ").upper())
 quantidade = float(input("Informe a quantidade: "))
+cartao_tabajara = str(input("Compra com Cartão Tabajara: (S-Sim ou N-Não): ").upper())
 
 if quantidade > 0:
-  if fruta == 'F' or 'A' or 'P':
-    if fruta == 'F':
-      if quantidade <= 5:
-        preco = quantidade * 2.50
-        print("Preço: R$ %.2f" % preco)
-      elif quantidade > 5 and quantidade <= 8:
-        preco = quantidade * 2.20
-        print("Preço: R$ %.2f" % preco)
-      else:
-        preco = (quantidade * 2.20)
-        if preco > 25:
-          desconto = preco * .10
+  if cartao_tabajara == 'S':
+    if fruta == ('F' or 'A' or 'P'):
+      if fruta == 'F':
+        if quantidade <= 5:
+          preco = quantidade * 4.90
+          desconto = preco * .05
           preco -= desconto
           print("Preço: R$ %.2f" % preco)
-        else:
-          print("Preço: R$ %.2f" % preco)
-    if fruta == 'A':
-      if quantidade > 0 and quantidade <= 5:
-        preco = quantidade * 1.80
-        print("Preço: R$ %.2f" % preco)
-      elif quantidade > 5 and quantidade <= 8:
-        preco = quantidade * 1.50
-        print("Preço: R$ %.2f" % preco)
-      else:
-        preco = (quantidade * 1.50)
-        if preco > 25:
-          desconto = preco * .10
+        elif quantidade > 5:
+          preco = quantidade * 5.80
+          desconto = preco * .05
           preco -= desconto
           print("Preço: R$ %.2f" % preco)
-        else:
+      if fruta == 'A':
+        if quantidade <= 5:
+          preco = quantidade * 5.90
+          desconto = preco * .05
+          preco -= desconto
+          print("Preço: R$ %.2f" % preco)
+        elif quantidade > 5:
+          preco = quantidade * 6.80
+          desconto = preco * .05
+          preco -= desconto
+          print("Preço: R$ %.2f" % preco)
+      if fruta == 'P':
+        if quantidade <= 5:
+          preco = quantidade * 6.90
+          desconto = preco * .05
+          preco -= desconto
+          print("Preço: R$ %.2f" % preco)
+        elif quantidade > 5:
+          preco = quantidade * 7.80
+          desconto = preco * .05
+          preco -= desconto
           print("Preço: R$ %.2f" % preco)
     else:
       print("\nERRO: Dado inválido\nEscolha: G-Morango ou A-Maçã")
+  elif cartao_tabajara == 'N':
+    if fruta == ('F' or 'A' or 'P'):
+      if fruta == 'F':
+        if quantidade <= 5:
+          preco = quantidade * 4.90
+          print("Preço: R$ %.2f" % preco)
+        elif quantidade > 5:
+          preco = quantidade * 5.80
+          print("Preço: R$ %.2f" % preco)
+      if fruta == 'A':
+        if quantidade <= 5:
+          preco = quantidade * 5.90
+          print("Preço: R$ %.2f" % preco)
+        elif quantidade > 5:
+          preco = quantidade * 6.80
+          print("Preço: R$ %.2f" % preco)
+      if fruta == 'P':
+        if quantidade <= 5:
+          preco = quantidade * 6.90
+          print("Preço: R$ %.2f" % preco)
+        elif quantidade > 5:
+          preco = quantidade * 7.80
+          print("Preço: R$ %.2f" % preco)
+    else:
+      print("\nERRO: Escolha: F-Filé Duplo, A-Alcatra, P-Picanha")
+  else:
+    print("Comando inválido. Escolha: (S-Sim ou N-Não")
 else:
   print("Quantidade tem que ser maior que 0")
