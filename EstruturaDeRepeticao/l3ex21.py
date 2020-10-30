287465e999dea28b641acead0287c6b2e943e5e8
rafaelmorgado <ecrafaelmorgado@gmail.com>
2020-10-30 14:46:26 +0000

    correcao

--- EstruturaDeRepeticao/l3ex21.py
+++ EstruturaDeRepeticao/l3ex21.py
@@ -1,20 +1,18 @@
 ''' 21) Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1. '''
 
-num = int(input("Entre com um número: "))
+numero = int(input("Entre com um número: "))
 
-if num == 1:
-  print("não é primo:", num)
+if numero == 0 or numero == 1:
+  print("não é primo:", numero)
 
-if num > 1:
-  eprimo = True
-  divisor = 2
-  while divisor <= num and eprimo:
-    if num % divisor == 0:
-      if num == 2:
-        eprimo = True
-      else:
-        eprimo = False
-        print("não é primo:",num)
-    if eprimo:
-      print("é primo:",num)
-      break
+if numero > 1:
+  divisores = 0
+  for divisor in range(1, numero):
+    if numero % divisor == 0:
+      divisores += 1
+      if divisores > 1:
+        break
+  if divisores > 1:
+    print(f"{numero} não é primo")
+  else:
+    print(f"{numero} é primo")
