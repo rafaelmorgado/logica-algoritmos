af050aa68683569678774414cc66369049c32d98
rafaelmorgado <ecrafaelmorgado@gmail.com>
2020-11-05 14:47:57 +0000

    correcao

--- EstruturaDeRepeticao/l3ex38.py
+++ EstruturaDeRepeticao/l3ex38.py
@@ -3,8 +3,8 @@ Esse funcionário foi contratado em 1995, com salário inicial de R$ 1.000,00;
 Em 1996 recebeu aumento de 1,5% sobre seu salário inicial;
 A partir de 1997 (inclusive), os aumentos salariais sempre correspondem ao dobro do percentual do ano anterior. Faça um programa que determine o salário atual desse funcionário. Após concluir isto, altere o programa permitindo que o usuário digite o salário inicial do funcionário. '''
 
-# salario = float(input("Entre com o salário: "))
-salario = 1000
+salario = float(input("Entre com o salário: "))
+
 ano = 1995
 percentual_aumento = .015
 percentual_aumento_recalculado_2 = percentual_aumento_recalculado = novo_salario = ano_salario = 0
